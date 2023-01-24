0f98898d68ef603a2701217bb00b7d56650e68b7
Thomas Matlock <32077179+zep1994@users.noreply.github.com>
2023-01-23 18:59:58 -0600

    Update read.py

--- read.py
+++ read.py
@@ -1,16 +1,24 @@
 import cv2 as cv
 
+#Read an Image
 img = cv.imread('Photos/cat_large.jpg')
+
+#Show Image
 cv.imshow('Cat', img)
 
-def rescaleFrame(frame, scale-0.75):
+#Rescale and Resize image
+def rescaleFrame(frame, scale=0.25):
     width = int(frame.shape[1] * scale)
     height = int(frame.shape[0] * scale)
 
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
 cv.waitKey(0)
 
-#READING VIDEO
-#capture = cv.VideoCapture("Videos/dog.mp4")
+# #READING VIDEO
+# capture = cv.VideoCapture("Videos/dog.mp4")
 
 # while True:
 #     isTrue, frame = capture.read()
