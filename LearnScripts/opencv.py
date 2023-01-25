4f474c81a78c544f0666d20d1112ac9ed752cd54
Thomas Matlock <32077179+zep1994@users.noreply.github.com>
2023-01-25 16:04:46 -0600

    Use hand.

--- LearnScripts/opencv.py
+++ LearnScripts/opencv.py
@@ -2,45 +2,45 @@
 import numpy as np
 import cv2 as cv
  
-# # initialize the HOG descriptor/person detector
-# hog = cv.HOGDescriptor()
-# hog.setSVMDetector(cv.HOGDescriptor_getDefaultPeopleDetector())
+# initialize the HOG descriptor/person detector
+hog = cv.HOGDescriptor()
+hog.setSVMDetector(cv.HOGDescriptor_getDefaultPeopleDetector())
 
-# cv.startWindowThread()
+cv.startWindowThread()
 
 # open webcam video stream
 cap = cv.VideoCapture(0)
 
-# the output will be written to output.avi
-# out = cv.VideoWriter(
-#     'output.avi',
-#     cv.VideoWriter_fourcc(*'MJPG'),
-#     15.,
-#     (640,480))
+#the output will be written to output.avi
+out = cv.VideoWriter(
+    'output.avi',
+    cv.VideoWriter_fourcc(*'MJPG'),
+    15.,
+    (640,480))
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    # # resizing for faster detection
-    # frame = cv.resize(frame, (640, 480))
-    # # using a greyscale picture, also for faster detection
-    # gray = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
+    # resizing for faster detection
+    frame = cv.resize(frame, (640, 480))
+    # using a greyscale picture, also for faster detection
+    gray = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
 
-    # detect people in the image
-    # returns the bounding boxes for the detected objects
-    # boxes, weights = hog.detectMultiScale(frame, winStride=(8,8) )
+#    detect people in the image
+    #returns the bounding boxes for the detected objects
+    boxes, weights = hog.detectMultiScale(frame, winStride=(8,8) )
 
-    # boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
 
-    # for (xA, yA, xB, yB) in boxes:
-    #     # display the detected boxes in the colour picture
-    #     cv.rectangle(frame, (xA, yA), (xB, yB),
-    #                       (0, 255, 0), 2)
+    for (xA, yA, xB, yB) in boxes:
+        # display the detected boxes in the colour picture
+        cv.rectangle(frame, (xA, yA), (xB, yB),
+                          (0, 255, 0), 2)
     
-    # # Write the output video 
-    #out.write(frame.astype('uint8'))
-    # Display the resulting frame
+    # Write the output video 
+    out.write(frame.astype('uint8'))
+  #  Display the resulting frame
     cv.imshow('frame',frame)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
